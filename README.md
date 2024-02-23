12307924d4f2b80a4e2518dde6de6e85f0365aa2
x22157271 <145765522+x22157271@users.noreply.github.com>
2024-02-23 12:39:55 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+
+###end to end ML project##
+
+### created and environment###
+```
+conda create -p venv python==3.8
+```
+```
+install all important libraries
+
+pip install -r requirement.txt
+```
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    requiremetns=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name="RegressorProject",
+    version="0.0.1",
+    author="Abhi",
+    author_email="abhijit0382@gmail.com",
+    install_requires=get_requirements('requirement.txt'),
+    packages=find_packages()
+)
